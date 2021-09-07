58cb3ed684b2fa467384a8c4288dccf2289cf195
makitasae <83483828+makitasae@users.noreply.github.com>
2021-09-07 09:58:28 +0900

    Add files via upload

---
+++ @about.php
@@ -0,0 +1,235 @@
+<?php
+
+include("../cls/CommonHtml.php");
+$ComHtml=new CommonHtml();
+
+?>
+<!doctype html>
+<html lang="ja">
+
+<head>
+  <?php echo $ComHtml->_Html_Head(2);?>
+</head>
+
+<body>
+
+<header role="banner">
+  <?php echo $ComHtml->_Html_Header(2); ?>
+</header> <!-- END header -->
+
+  <section class="home-slider-loop-false  inner-page owl-carousel">
+    <div class="slider-item" style="background-image: url('../img/hero_2.jpg');">
+
+      <div class="container">
+        <div class="row slider-text align-items-center justify-content-center">
+          <div class="col-md-8 text-center col-sm-12 element-animate">
+            <h1>About Us</h1>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </section>
+
+  <div class="section container">
+    <div class="row">
+      <div class="col-lg-6">
+        <p><img src="../img/hero_2.jpg" alt="" class="img-fluid"></p>
+        <p class="mb-5"><img src="../img/hero_1.jpg" alt="" class="img-fluid"></p>
+      </div>
+
+      <div class="col-lg-6 pl-2 pl-lg-5">
+        <h2 class="mb-5">History</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius est soluta blanditiis velit doloremque
+          corrupti aliquid ducimus consectetur ea nobis dolorem, id quibusdam praesentium consequuntur modi eligendi,
+          sunt suscipit ullam iure nesciunt tempore. Itaque placeat, libero aliquam odio ex voluptas.</p>
+        <p>Vel vitae, assumenda blanditiis nemo in vero reprehenderit asperiores distinctio exercitationem aliquid, quam
+          velit explicabo neque. Sapiente provident sequi omnis itaque eaque voluptatum vel. Accusamus deserunt atque
+          eligendi mollitia voluptates eum libero, ratione id labore. Magnam porro dolorem aspernatur, dolor?</p>
+
+        <p class="mb-5">Vel vitae, assumenda blanditiis nemo in vero reprehenderit asperiores distinctio exercitationem
+          aliquid, quam velit explicabo neque. Sapiente provident sequi omnis itaque eaque voluptatum vel. Accusamus
+          deserunt atque eligendi mollitia voluptates eum libero, ratione id labore. Magnam porro dolorem aspernatur,
+          dolor?</p>
+
+      </div>
+    </div>
+  </div>
+
+  </section>
+
+
+
+
+
+  <section class="section element-animate">
+
+    <div class="clearfix mb-5 pb-5">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12 text-center heading-wrap">
+            <h2>Chefs</h2>
+            <span class="back-text">Our Chefs</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="major-caousel js-carousel-1 owl-carousel">
+          <div>
+            <div class="media d-block media-custom text-center">
+              <a href="adoption-single.html"><img src="../img/person_1.jpg" alt="Image Placeholder"
+                  class="img-fluid"></a>
+              <div class="media-body">
+                <h3 class="mt-0 text-black">Mellisa Howard</h3>
+                <p class="lead">CEO, Co-Founder</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="media d-block media-custom text-center">
+              <a href="adoption-single.html"><img src="../img/person_2.jpg" alt="Image Placeholder"
+                  class="img-fluid"></a>
+              <div class="media-body">
+                <h3 class="mt-0 text-black">Mike Richardson</h3>
+                <p class="lead">CEO, Co-Founder</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="media d-block media-custom text-center">
+              <a href="adoption-single.html"><img src="../img/person_3.jpg" alt="Image Placeholder"
+                  class="img-fluid"></a>
+              <div class="media-body">
+                <h3 class="mt-0 text-black">Charles White</h3>
+                <p class="lead">CEO, Co-Founder</p>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div class="media d-block media-custom text-center">
+              <a href="adoption-single.html"><img src="../img/person_1.jpg" alt="Image Placeholder"
+                  class="img-fluid"></a>
+              <div class="media-body">
+                <h3 class="mt-0 text-black">Mellisa Howard</h3>
+                <p class="lead">CEO, Co-Founder</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="media d-block media-custom text-center">
+              <a href="adoption-single.html"><img src="../img/person_2.jpg" alt="Image Placeholder"
+                  class="img-fluid"></a>
+              <div class="media-body">
+                <h3 class="mt-0 text-black">Mike Richardson</h3>
+                <p class="lead">CEO, Co-Founder</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="media d-block media-custom text-center">
+              <a href="adoption-single.html"><img src="../img/person_3.jpg" alt="Image Placeholder"
+                  class="img-fluid"></a>
+              <div class="media-body">
+                <h3 class="mt-0 text-black">Charles White</h3>
+                <p class="lead">CEO, Co-Founder</p>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+        <!-- END slider -->
+      </div>
+    </div>
+
+  </section> <!-- .section -->
+
+
+
+  <footer class="site-footer" role="contentinfo">
+    <div class="container">
+      <div class="row mb-5">
+        <div class="col-md-4 mb-5">
+          <h3>About Us</h3>
+          <p class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus et dolor blanditiis
+            consequuntur ex voluptates perspiciatis omnis unde minima expedita.</p>
+          <ul class="list-unstyled footer-link d-flex footer-social">
+            <li><a href="#" class="p-2"><span class="fa fa-twitter"></span></a></li>
+            <li><a href="#" class="p-2"><span class="fa fa-facebook"></span></a></li>
+            <li><a href="#" class="p-2"><span class="fa fa-linkedin"></span></a></li>
+            <li><a href="#" class="p-2"><span class="fa fa-instagram"></span></a></li>
+          </ul>
+
+        </div>
+        <div class="col-md-5 mb-5">
+          <div class="mb-5">
+            <h3>Opening Hours</h3>
+            <p><strong class="d-block">Sunday-Thursday</strong> 5AM - 10PM</p>
+          </div>
+          <div>
+            <h3>Contact Info</h3>
+            <ul class="list-unstyled footer-link">
+              <li class="d-block">
+                <span class="d-block">Address:</span>
+                <span class="text-white">34 Street Name, City Name Here, United States</span></li>
+              <li class="d-block"><span class="d-block">Telephone:</span><span class="text-white">+1 242 4942 290</span>
+              </li>
+              <li class="d-block"><span class="d-block">Email:</span><span class="text-white">info@yourdomain.com</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-3 mb-5">
+          <h3>Quick Links</h3>
+          <ul class="list-unstyled footer-link">
+            <li><a href="#">About</a></li>
+            <li><a href="#">Terms of Use</a></li>
+            <li><a href="#">Disclaimers</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12 text-md-center text-left">
+          <p>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            Copyright &copy;
+            <script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with
+            <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com"
+              target="_blank">Colorlib</a>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- END footer -->
+
+  <!-- loader -->
+  <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
+      <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
+      <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10"
+        stroke="#cf1d16" /></svg></div>
+
+  <script src="../js/jquery-3.2.1.min.js"></script>
+  <script src="../js/popper.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+  <script src="../js/owl.carousel.min.js"></script>
+  <script src="../js/jquery.waypoints.min.js"></script>
+
+  <script src="../js/jquery.magnific-popup.min.js"></script>
+  <script src="../js/magnific-popup-options.js"></script>
+
+
+  <script src="../js/main.js"></script>
+</body>
+
+</html>
